af06d537c3f2d4a36c1417fee34d3feb8326d2a7
veronica salazar <vsalazar@datastar.com.ar>
2020-01-20 12:33:07 -0300

    COMMIT PARCIAL

--- miSanMartin/src/app/home/home.page.ts
+++ miSanMartin/src/app/home/home.page.ts
@@ -7,11 +7,22 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      disableOnInteraction: false
+    }
+  };
+  featured: any;
   constructor(public navCtrl: NavController) {
-
+    this.featured = [
+      {title: "Exploring San Francisco", author: "Rea Ramsey", body: "", picture: "https://picsum.photos/500/400?image=693"},
+      {title: "Coffee the right way", author: "Ellesha Hartley", body: "", picture: "https://picsum.photos/500/400?image=1060"},
+      {title: "Best Hiking In Yosemite", author: "Vinnie Alexander", body: "", picture: "https://picsum.photos/500/400?image=1043"},
+      {title: "Astro Photography Guide", author: "Greg Rakozy", body: "", picture: "https://picsum.photos/500/400?image=903"}
+    ];
 
   }
-
 }
